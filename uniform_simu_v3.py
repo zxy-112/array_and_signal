36885eda377f9c74d3e05490b38914c2331db2ad
zxy <775847012@qq.com>
2022-04-02 11:09:54 +0800

    重写了均匀阵列仿真部分的代码

---
+++ uniform_simu_v3.py
@@ -0,0 +1,76 @@
+import os
+from collections import Iterable
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+from cycler import cycler
+import aray
+import signl
+from utils import *
+
+SAVE_PATH = os.path.join(os.path.expanduser('~'), 'coherent_simu_results')
+if not os.path.exists(SAVE_PATH):
+    os.mkdir(SAVE_PATH)
+
+ele_num = 16
+sample_points = 1000
+coherent_theta = 20
+cnr = -10
+incoherent_theta = -30
+inr = 10
+expect_theta = 0
+snr = 0
+
+ary = aray.UniformLineArray()
+decibel2val = lambda x: np.sqrt(np.power(10, x / 10) * ary.noise_power)
+for k in range(ele_num):
+    ary.add_element(aray.Element())
+signals = [signl.LfmWave2D(theta=expect_theta, amplitude=decibel2val(snr)),
+        signl.LfmWave2D(theta=coherent_theta, signal_type='coherent_interference', amplitude=decibel2val(cnr), phase_shift=0.2 * np.pi),
+        signl.LfmWave2D(theta=incoherent_theta, signal_type='interference', delay=1e-6, amplitude=decibel2val(inr))]
+for signal in signals:
+    ary.receive_signal(signal)
+ary.sample(sample_points)
+
+output = ary.output
+
+weight = ary.steer_vector(expect_theta)
+adaptive_weight = mvdr(output, expect_theta, ary.steer_vector)
+mcmv_weight = mcmv(output, expect_theta, coherent_theta, ary.steer_vector)
+ctmv_weight = ctmv(output, expect_theta, coherent_theta, ary.steer_vector, ary.noise_power)
+ctp_weight = ctp(output, expect_theta, coherent_theta, ary.steer_vector, ary.noise_power)
+
+color=['#037ef3', '#f85a40', '#00c16e', '#7552cc', '#0cb9c1', '#f48924', '#ffc845', '#52565e']
+lw = [2, 2, 2, 2] * 2
+linestyle = [
+        (0, ()),
+        (0, (1, 1)),
+        (0, (3, 1, 1, 1)),
+        (0, (3, 1, 1, 1, 1, 1))
+        ]
+linestyle = linestyle * 2
+assert len(color) == len(lw) == len(linestyle)
+custom_cycler = (cycler(color=color) +
+                 cycler(lw=lw) +
+                 cycler(linestyle=linestyle))
+all_lines = [mlines.Line2D([], [], **config) for config in custom_cycler]
+methods = ['MVDR', 'MCMV', 'CTMV', 'CTP']
+# 响应
+plt.rc('axes', prop_cycle=custom_cycler)
+with plt.ioff():
+    fig_ax = plt.subplots()
+ary.response_plot(adaptive_weight, fig_ax_pair=fig_ax)
+ary.response_plot(mcmv_weight, fig_ax_pair=fig_ax)
+ary.response_plot(ctmv_weight, fig_ax_pair=fig_ax)
+ary.response_plot(ctp_weight, fig_ax_pair=fig_ax)
+fig_ax[0].legend(all_lines[:len(methods)], methods)
+
+# 波束形成
+def normalize(x):
+    return x / np.max(x)
+
+weights = [adaptive_weight, mcmv_weight, ctmv_weight, ctp_weight]
+for item in weights:
+    my_plot(normalize(np.real(syn(item, output))))
+
+plt.show()
